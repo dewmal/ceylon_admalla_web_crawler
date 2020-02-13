e1d16b7b43b86b388f9997b760910deec773eb5b
Dewmal <dewmalnilanka@gmail.com>
2020-02-13 20:05:41 +0530

    Ikman crawler run

--- classified_web_crawler/classified_web_crawler/spiders/ikman.py
+++ classified_web_crawler/classified_web_crawler/spiders/ikman.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.linkextractors import LinkExtractor
 
 
 class IkmanSpider(scrapy.Spider):
@@ -8,4 +9,18 @@ class IkmanSpider(scrapy.Spider):
     start_urls = ['http://ikman.lk/']
 
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+
+        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
+
+        # next_urls = response.css('a').xpath('@href').getall()
+        # if next_urls:
+        #     for next_url in next_urls:
+        #         pass
+        #
+        #     next_page = ""
+
+        for link in links:
+            next_page = link.url
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
---
+++ main.py
@@ -0,0 +1,11 @@
+from scrapy.crawler import CrawlerProcess
+
+from classified_web_crawler.classified_web_crawler.spiders.ikman import IkmanSpider
+
+process = CrawlerProcess(settings={
+    'FEED_FORMAT': 'json',
+    'FEED_URI': 'items.json'
+})
+
+process.crawl(IkmanSpider)
+process.start()  # the script will block here until the crawling is finished
